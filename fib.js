75ddee3f78e2a81e99534aceff13320b91e88673
NUG01 <nskhiereli@gmail.com>
2022-08-12 21:48:02 +0400

    initial commit

---
+++ fib.js
@@ -0,0 +1,79 @@
+const change = document.querySelector(".currentYear");
+const year = new Date().getFullYear();
+change.textContent = year;
+
+const menu = document.querySelector(".js-btn");
+const header = document.querySelector(".header");
+menu.addEventListener("click", function () {
+  header.classList.toggle("nav-open");
+});
+
+const allLinks = document.querySelectorAll("a:link");
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    if (href !== "#" && href.startsWith("#")) {
+      const section = document.querySelector(href);
+      section.scrollIntoView({
+        behavior: "smooth",
+      });
+    }
+    if (link.classList.contains("navs")) header.classList.toggle("nav-open");
+  });
+});
+const hero = document.querySelector(".section-hero");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    console.log(ent);
+    if (ent.isIntersecting === false) document.body.classList.add("sticky");
+    if (ent.isIntersecting === true) document.body.classList.remove("sticky");
+  },
+  {
+    root: null,
+    threshold: 0,
+    rootMargin: "-70px",
+  }
+);
+obs.observe(hero);
+const links = document.querySelectorAll(".nav ul li a");
+
+for (const link of links) {
+  link.addEventListener("click", clickHandler);
+}
+
+function clickHandler(e) {
+  e.preventDefault();
+  const href = this.getAttribute("href");
+  const offsetTop = document.querySelector(href).offsetTop;
+
+  scroll({
+    top: offsetTop,
+    behavior: "smooth",
+  });
+}
+
+function checkFlexGap() {
+  var flex = document.createElement("div");
+  flex.style.display = "flex";
+  flex.style.flexDirection = "column";
+  flex.style.rowGap = "1px";
+
+  flex.appendChild(document.createElement("div"));
+  flex.appendChild(document.createElement("div"));
+
+  document.body.appendChild(flex);
+  var isSupported = flex.scrollHeight === 1;
+  flex.parentNode.removeChild(flex);
+  console.log(isSupported);
+
+  if (!isSupported) document.body.classList.add("no-flexbox-gap");
+}
+checkFlexGap();
